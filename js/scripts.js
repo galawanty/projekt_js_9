f3f8c112aae74f82bfbd4bf8ba93c6da6dca09be
Łukasz Wasiluk <galawanty@gmail.com>
2017-03-11 23:38:13 +0100

    zadanie 9

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+//var txt = document.getElementsByTagName('p');
+//console.log(txt.innerText);
+
+var txt = document.getElementsByClassName('btn');
+console.log(txt);
+console.log(txt.length);
+for ( var b = 0 ; b < txt.length ; b++) {
+  console.log(+ b);
+function buttons() {
+	var buttons = document.getElementsByTagName('button');
+}
+console.log(buttons.innerText);
+}
+
